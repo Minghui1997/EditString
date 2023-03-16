5c168f46c4e060ca246a6630498fb08650dc2a78
MingHui <103197921+MingHui1997@users.noreply.github.com>
2023-03-16 22:35:25 +0800

    Add files via upload

---
+++ EditString/EditString.h
@@ -0,0 +1,10 @@
+﻿#include <stddef.h>
+#include <string.h>
+
+void strslice(char *dest,char *sour,size_t s,size_t e);
+void strclear(char *dest);
+size_t charindex(char *dest,char fc);
+size_t charindex_last(char *dest,char fc);
+size_t strindex(char *dest,char *str);
+size_t strindex_last(char *dest,char *str);
+void strreplace(char *dest,char *repstr,char *sour);
